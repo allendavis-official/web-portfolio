41b3cfc390698611a6450ca9ace6dbf27b3aacf2
allendavis-official <allendavis_official@hotmail.com>
2025-05-21 19:55:57 +0000

    Feature: Navbar added to UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
 import "./styles/App.scss";
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <section className="hero">
         <h1>Hi, I'm Allen Davis</h1>
         <p>Full-Stack Developer building web apps with React & Node.js</p>
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+// src/components/Header.js
+import React from "react";
+import { Link } from "react-router-dom"; // We'll add routing later
+
+const Header = () => {
+  return (
+    <header className="navbar">
+      <nav>
+        <Link to="/" className="nav-logo">
+          YourName
+        </Link>
+        <div className="nav-links">
+          <Link to="#projects">Projects</Link>
+          <Link to="#about">About</Link>
+          <Link to="#contact">Contact</Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
